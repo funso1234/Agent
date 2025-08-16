7c6d1a1af1dac39b8dedd84700855fd2ac4cddeb
funso1234 <samuel08063251499@gmail.com>
2025-08-17 00:26:53 +0100

    working on contact us component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
       <Services/>
       <OurWork/>
       <Teams/>
+      <ContactUs/>
     </div>
   )
 }
