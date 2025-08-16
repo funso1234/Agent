a165d4bc493327bb56be86effca2cb0af9eb857e
funso1234 <samuel08063251499@gmail.com>
2025-08-16 02:22:25 +0100

    done with services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
+import ServicesCard from './ServicesCard'
 
 const Services = () => {
 
@@ -34,6 +35,12 @@ const Services = () => {
         <Title title='How can we help?' desc='From strategy to
         execution, we craft digital solutions that move your business
         forward.'/>
+
+        <div className='flex flex-col md:grid grid-cols-2'>
+            {servicesData.map((service, index)=>(
+                <ServicesCard key={index} service={service} index={index}/>
+            ))}
+        </div>
 
     </div>
   )
